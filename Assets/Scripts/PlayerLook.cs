88bd4b4e967df916b6c4b262cb51c0579a4504e6
jakublel <jakublelek69@gmail.com>
2023-03-22 20:09:44 +0100

    kontroler gracza cz2
    jest poruszanie, rozgladanie, grawitacja

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    public float sensivity = 300;
+
     float xRotation = 0;
+    Transform player;
     void Start()
     {
-        
+        player = transform.parent;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensivity;
 
         xRotation -= mouseY;
-
+        xRotation = Mathf.Clamp(xRotation, -70, 70);
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        player.Rotate(0, mouseX, 0);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     CharacterController controller;
 
     public float speed = 5;
+    public LayerMask groundMask;
+    public MeshRenderer playerRenderer;
+    bool grounded;
 
     private void Start()
     {
@@ -15,14 +18,52 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        Movement();
+        GroundCheck();
+        Gravity();
+    }
+
+
+    float velocityY;
+    void Gravity()
+    {
+        if (!grounded)
+        {
+            velocityY += Physics.gravity.y * Time.deltaTime;
+            controller.Move(new Vector3(0, velocityY, 0) * Time.deltaTime);
+        }
+        else
+        {
+            velocityY = 0;
+        }
+    }
+    void GroundCheck()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        grounded = Physics.SphereCast(ray, 0.5f, out hit, 0.6f, groundMask.value);
+
+        if (grounded)
+        {
+            playerRenderer.material.color = Color.green;
+        }
+        else
+        {
+            playerRenderer.material.color = Color.red;
+        }
+    }
+    void Movement()
+    {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
-        Vector3 move = new Vector3(inputX, 0, inputY);
+
+        Vector3 move = transform.TransformDirection(new Vector3(inputX, 0, inputY));
 
         if (move.magnitude > 1)
             move = move.normalized;
-        
+
         controller.Move(move * Time.deltaTime * speed);
-        Debug.Log(inputX);
     }
+    
+
 }
